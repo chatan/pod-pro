4c78c9d8ec6da154f57199ee0dc4552a56f1f407
chatan <okichatan@gmail.com>
2017-03-04 16:54:03 +0900

    copy direct to usb

--- pod-pro-mover-and-splitter.sh
+++ pod-pro-mover-and-splitter.sh
@@ -2,23 +2,27 @@
 # a script to recursively find and copy files to a desired location  
 # even works with files with $ or spaces
 #
-minutes=$(whiptail --title "Podcast Processor" --inputbox "Enter minute split with decimal; e.g. 3.0" 10 40 3>&1 1>&2 2>&3)
+minutes=$(whiptail --title "Podcast Processor" --inputbox "Enter split in minutes with decimal; e.g. 3.0 " 10 40 3>&1 1>&2 2>&3)
+# this gets mount point for the usb sdd1:
+usb=$(findmnt -nr -o target -S /dev/sdd1)
 #
+if (whiptail --title "Podcast Processor" --yesno "Confirm that the usb in mounted at /dev/sdd1 is $usb" 10 60) 
+then
+whiptail --title "Podcast Processor" --msgbox "Confirm USB usb not open in Dolphin" 10 30
 find ~/gPodder -type f -iname '*.mp3' -print0 |
 while IFS= read -r -d '' f; 
 do 
 #    echo ****************  processing filename=$f
 #    cp -- "$f" /media/user/VIDEO-ETC/podcasts-for-car ;
     filename=$(basename "$f")
-    short=${filename:0:3}
-    echo  $f
-    echo
-    echo  $filename
-    echo
-    echo   short folder name = $short
-    echo
-# since the gpodder folders have spaces, the $f needs to be in quotes, else error
-    mp3splt -f -t $minutes -a -d $short "$f"
-done 
-#
-#
+    short=$(whiptail --title "Podcast Processor" --inputbox "Enter a short name for $filename " 10 40 3>&1 1>&2 2>&3)    
+# for next mp3splt command:
+# -  since the gpodder folders have spaces, the $f needs to be in quotes, else error
+# -  custom name with the -o option:
+    mp3splt -f -t $minutes -a -d $usb -o $short@n "$f"
+done
+umount /dev/sdd1
+sudo fatsort /dev/sdd1
+else
+ echo "Exit"
+fi
